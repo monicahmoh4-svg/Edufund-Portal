024542ce92043f6abdfa5c7db84b44b6d450f5a1
monicahmoh4-svg <monicahmoh4@gmail.com>
2026-05-27 21:56:59 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
       { protocol: 'https', hostname: 'picsum.photos' },
     ],
   },
